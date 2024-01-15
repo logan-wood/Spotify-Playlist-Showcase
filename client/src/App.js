2783e8310a772dcdac28e09b3fa8a9880441aed7
logan-wood <logan.loganwood@gmail.com>
2024-01-15 23:51:04 +1300

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import spotifyLogo from './images/spotify_logo.png'
+import './App.css';
+
+function App() {
+  const spotifyLogin = () => {
+    console.log('Logging in with spotify')
+  }
+
+  return (
+    <div className="App">
+      <nav>
+        <h4>Showcaser</h4>
+        <a>About</a>
+      </nav>
+      
+      <section>
+        <div className="info">
+          <h1>Showcase your playlists</h1>
+          <p>Showcaser is the best way to show your playlists to other people. You choose how other people hear your playlists - choose which tracks to show in which order and which snippits to play.</p>
+        </div>
+        
+        <div className="login" onClick={spotifyLogin} >
+          <img src={spotifyLogo} className="spotify-logo"></img>
+          <h4>Login With Spotify</h4>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
